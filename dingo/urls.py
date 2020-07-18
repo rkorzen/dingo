3024e9b607e199308df34e3e5e8f7b5a3865bf03
rkorzen <korzeniewski@gmail.com>
2020-07-18 16:56:10 +0200

    Wprowadzamy zmienne do ścieżki
    Dzięki wprowadzeniu zmiennych do ścieżki nasza ścieżka poradzi sobie z różnymi wartościami
    
    poprzednio:
    
        path('maths/add/1/2', lambda request: HttpResponse(3)),
    
    taka ścieżka obsłuży tylko bardzo konkretny przypadek.
    
        http://localhost:8000/maths/add/1/2
    
    inna ścieżka, np:
    
        http://localhost:8000/maths/add/3/5
    
    spowoduje wyjątek
    
    Wprowadzenie zmiennych pozwoli nam na bardziej uniwersalne użycia:
    
        path('maths/add/<a>/<b>', lambda request, a, b: HttpResponse(a+b)),
    
    Teraz a i b mogą przyjąć w zasadzie dowolne wartości.
    
    Co akurat w tym przypadku nie zawsze jest uzasadnione...

--- dingo/urls.py
+++ dingo/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('maths/', lambda request: HttpResponse("Tu będzie matma")),
+    path('maths/add/<a>/<b>', lambda request, a, b: HttpResponse(a + b)),
 ]
