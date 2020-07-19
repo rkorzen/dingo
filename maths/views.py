70fab47bd843569f2b1686aad28e11724646b441
rkorzen <korzeniewski@gmail.com>
2020-07-19 21:43:07 +0200

    Dodanie szablony html i wypełnienie go kontekstem
    W  obecnej formie kod html miesza się z kodem pythona.
    Nie jest to najlepsze rozwiązanie
    
    W kolejnym kroku poprawimy to.

--- maths/views.py
+++ maths/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Context, Template
 
 
 def math(request):
@@ -8,7 +9,21 @@ def math(request):
 
 def add(request, a, b):
     a, b = int(a), int(b)
-    return HttpResponse(a + b)
+    wynik = a + b
+    t = Template("""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+Wynik operacji {{a}} + {{b}} wynosi {{wynik}}    
+</body>
+</html>
+        """)
+    c = Context({"a": a, "b": b, "wynik": wynik})
+    return HttpResponse(t.render(c))
 
 
 def sub(request, a, b):
