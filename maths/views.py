e8f1a041489a88f76c93f6512550c68b23131b6f
rkorzen <korzeniewski@gmail.com>
2020-07-19 22:21:04 +0200

    Załadowanie szablonu z pliku i wypełnienie go treścią

--- maths/views.py
+++ maths/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import Context, Template
+from django.template import Context, loader
 
 
 def math(request):
@@ -10,19 +10,8 @@ def math(request):
 def add(request, a, b):
     a, b = int(a), int(b)
     wynik = a + b
-    t = Template("""
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-Wynik operacji {{a}} + {{b}} wynosi {{wynik}}    
-</body>
-</html>
-        """)
-    c = Context({"a": a, "b": b, "wynik": wynik})
+    t = loader.get_template("maths/main.html")
+    c = {"a": a, "b": b, "operacja": "+", "wynik": wynik}
     return HttpResponse(t.render(c))
 
 
